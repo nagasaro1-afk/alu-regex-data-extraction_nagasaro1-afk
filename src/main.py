9d6b4b1e4f763eb0e3270d25aec8ff1c9fbaa98a
nagasaro1-afk <n.agasaro1@alustudent.com>
2026-09-06 21:25:16 +0200

    start project, read the input file and split into lines

---
+++ src/main.py
@@ -0,0 +1,20 @@
+﻿import re
+import json
+
+input_path = "../input/raw-text.txt"
+output_path = "../output/sample-output.json"
+max_size = 50000
+
+print("Reading input file...")
+
+f = open(input_path, "r", encoding="utf-8")
+text = f.read()
+f.close()
+
+if len(text) > max_size:
+    print("file too big, stopping")
+    exit()
+
+lines = text.split("\n")
+
+print("Done reading, got " + str(len(lines)) + " lines")
